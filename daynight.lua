8439880ea2e8be9a833762d4dd2f0306e054da47
jefff2000 <68418829+jefff2000@users.noreply.github.com>
2021-03-22 01:12:00 +0800

    Update daynight.lua

--- daynight.lua
+++ daynight.lua
@@ -3,12 +3,6 @@ local rus = game:GetService("RunService")
 local day = 1440
 local interval = 1
 while true do
-  lastsec = tick()
-  while true do
-    rus.Heartbeat:Wait()
-    if tick()-lastsec >= interval then
-      break
-    end
-  end
+  wait(interval)
   light:SetMinutesAfterMidnight((light:GetMinutesAfterMidnight()+interval)%day)
 end
